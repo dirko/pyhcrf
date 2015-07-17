80c5554a09f869fe11c11c1482c6d0559c7dbff6
Dirko Coetsee <dirko@ml.sun.ac.za>
2015-07-17 22:18:28 +0200

    Finished forward-backward test. Removed old code. Started adding fit method. Added predict and predict_proba.

--- pyhcrf/hcrf.py
+++ pyhcrf/hcrf.py
@@ -20,18 +20,24 @@ class Hcrf(object):
     Includes methods for training using LM-BFGS, scoring, and testing, and
     helper methods for loading and saving parameter values to and from file.
     """
-    def __init__(self, S, l2_regularization=0.0):
+    def __init__(self,
+                 num_states=2,
+                 l2_regularization=0.0,
+                 transitions=None,
+                 state_parameter_noise=0.001,
+                 transition_parameter_noise=0.001):
         """
-        Initialize new HCRF object with hidden units with cardinality S.
+        Initialize new HCRF object with hidden units with cardinality `num_states`.
         """
         self.lamb = l2_regularization
-
-        self.S = S
-
-        self.A = None
-        self.B = None
-        self.C = None
-        self.D = None
+        assert(num_states > 0)
+        self.num_states = num_states
+        self.classes_ = None
+        self.state_parameters = None
+        self.transition_parameters = None
+        self.transitions = transitions
+        self.state_parameter_noise = state_parameter_noise
+        self.transition_parameter_noise = transition_parameter_noise
 
     def fit(self, X, y):
         """Fit the model according to the given training data.
@@ -49,34 +55,30 @@ class Hcrf(object):
         self : object
             Returns self.
         """
-        self.X = X
-        self.Y = y
+        classes = list(set(y))
+        num_classes = len(classes)
+        self.classes_ = classes
+        if self.transitions is None:
+            self.transitions = self._create_default_transitions(num_classes, self.num_states)
 
-        F = max(max(max(eeex for eeex in eex) for eex in ex) for ex in X) + 1
-        W = max(y) + 1
-        self.W = W
-        S = self.S
+        # Initialise the parameters
+        _, num_features = X[0].shape
+        num_transitions, _ = self.transitions.shape
+        if self.state_parameters is None:
+            self.state_parameters = numpy.random.standard_normal((num_features,
+                                                                  self.num_states + 2,
+                                                                  num_classes)) * self.state_parameter_noise
+        if self.transition_parameters is None:
+            self.transition_parameters = numpy.random.standard_normal((num_transitions)) * self.transition_parameter_noise
 
-        # Initialize the parameters uniformly random on [0, 0.1]
-        seed(2)
-        self.param = numpy.array([random() * 0.1 for i in xrange(S * S + S * W * F)])
-        for s in xrange(S):
-            for ps in xrange(S):
-                # Make it impossible for hidden units to remain in state 0.
-                if ps == 0 and s == 0:
-                    self.param[s * S + ps] = -numpy.inf
-                # Hidden units can also not stay in last state S-1.
-                if ps == S - 1 and s == S - 1:
-                    self.param[s * S + ps] = -numpy.inf
-                # Hidden states can only go to higher state or stay in the
-                # same state.
-                if s < ps:
-                    self.param[s * S + ps] = -numpy.inf
-        self.param_non_inf_indexes = [i for i in xrange(len(self.param)) if self.param[i] != -numpy.inf]
+        def objective_function(parameter_vector):
+            ll = 0.0
+            grad = numpy.zeros_like(parameter_vector)
+            for x, ty in zip(X, y):
+                dll, dgradient = log_likelihood(x, ty)
+            return -ll, -grad
 
-        # Train
-        final_params = self.train_lmbfgs()
-        self.param[self.param_non_inf_indexes] = final_params[0]
+        self._optimizer_result = fmin_l_bfgs_b(objective_function, initial_parameter_vector, **optimizer_kwargs)
 
     def predict(self, X):
         """Predict the class for X.
@@ -92,314 +94,82 @@ class Hcrf(object):
         y : iterable of shape = [n_samples]
             The predicted classes.
         """
+        return [self.classes[prediction.argmax()] for prediction in self.predict_proba(X)]
+
+    def predict_proba(self, X):
+        """Probability estimates.
+
+        The returned estimates for all classes are ordered by the
+        label of classes.
+
+        Parameters
+        ----------
+        X : List of ndarrays, one for each training example.
+
+        Returns
+        -------
+        T : array-like, shape = [n_samples, n_classes]
+            Returns the probability of the sample for each class in the model,
+            where classes are ordered as they are in ``self.classes_``.
+        """
         y = []
         for x in X:
-            pred = self.load_example(x)
-            py = max(zip(pred, range(len(pred))), key=lambda element: element[0])[1]
-            y.append(py)
-        return y
+            forward_table, _, _ = forward_backward(x,
+                                                   self.state_parameters,
+                                                   self.transition_parameters,
+                                                   self.transitions)
+            y.append(numpy.exp(forward_table[-1, -1, :]))
+        return numpy.array(y)
 
-    def reset_tables(self):
-        T = self.T
-        S = self.S
-        W = self.W
-        self.A = numpy.zeros((T, S, W))
-        self.B = numpy.zeros((T, S, W))
-        self.C = numpy.zeros((T, S, W))
-        self.D = numpy.zeros((T, S, S, W))
-
-    def reset_deriv(self):
-        self.der = numpy.zeros(len(self.param))
-        self.ll = 0.0
-
-    def load_example(self, x):
-        # Fill forward, backward, and combination tables for example x
-        self.T = len(x)
-        self.reset_tables()
-        self.x = x
-        self.fill_A()
-        self.fill_B()
-        self.fill_C()
-        # Return the predicted distribution over classes
-        return [sum(self.C[self.T - 1, :, y]) for y in xrange(self.W)]
-
-    def get_deriv(self, x, y):
-        # Return the log-likelihood and derivative of the parameters for
-        # a training example with features x and label y.
-        self.load_example(x)
-        self.fill_D()
-        D = self.D
-        W = self.W
-        S = self.S
-        T = self.T
-        C = self.C
-        der = self.der
-        ll = numpy.log(sum(C[T - 1, :, y]))
-        self.ll += ll
-
-        # Factors without output variable interaction
-        for t in xrange(T):
-            E_ef_norm = sum(C[t, :, y])
-            for w in xrange(W):
-                for s in xrange(S):
-
-                    E_f = C[t, s, w]
-                    E_ef = C[t, s, w]
-                    if w != y:
-                        E_ef = 0
-                    f_list = self.get_fs(t, s, w)
-                    E_ef_n = E_ef / E_ef_norm
-
-                    for f in f_list:
-                        der[f] += E_ef / E_ef_norm - E_f
-        # Factors containing two output variables
-        for t in xrange(T - 1):
-            E_ef_norm = sum(sum(D[t, :, :, y]))
-            for w in xrange(W):
-                for s in xrange(S):
-                    for ps in xrange(S):
-                        E_f = D[t, s, ps, w]
-                        E_ef = D[t, s, ps, w]
-                        if w != y:
-                            E_ef = 0
-                        f_list = self.get_fss(t, s, ps, w)
-                        E_ef_n = E_ef / E_ef_norm
-
-                        for f in f_list:
-                            der[f] += E_ef_n - E_f
-
-        return ll, der
-
-    def fill_A(self):
-        # Fill the forward table.
-        T = self.T
-        S = self.S
-        W = self.W
-        A = self.A
-        for w in xrange(W):
-            A[0, 0, w] = numpy.exp(self.get_f_single(0, 0, w))
-
-        for t in xrange(1, T):
-            for w in xrange(W):
-                for s in xrange(S):
-                    for ps in xrange(S):
-                        A[t, s, w] = A[t, s, w] + A[t - 1, ps, w] * numpy.exp(self.get_f(t, s, ps, w))
-            norm = sum(sum(A[t, :, :]))
-            A[t, :, :] /= norm
-
-    def fill_B(self):
-        # Fill the backward table.
-        T = self.T
-        S = self.S
-        W = self.W
-        B = self.B
-        for w in xrange(W):
-            B[T - 1, S - 1, w] = numpy.exp(self.get_f_single(T - 1, S - 1, w))
-        for t in xrange(T - 2, -1, -1):
-            for w in xrange(W):
-                for s in xrange(S):
-                    for ps in xrange(S):
-                        B[t, ps, w] = B[t, ps, w] + B[t + 1, s, w] * numpy.exp(self.get_f(t + 1, s, ps, w))
-            norm = sum(sum(B[t, :, :]))
-            B[t, :, :] /= norm
-
-    def fill_C(self):
-        # Fill and normalize table with product of forward and backward tables.
-        self.C = self.A * self.B
-        for t in xrange(self.T):
-            norm = sum(sum(self.C[t, :, :]))
-            self.C[t, :, :] = self.C[t, :, :] / norm
-
-    def fill_D(self):
-        # Get probability table.
-        T = self.T
-        S = self.S
-        W = self.W
-        A = self.A
-        B = self.B
-        D = self.D
-        for t in xrange(T - 1):
-            for w in xrange(W):
-                for s in xrange(S):
-                    for ps in xrange(S):
-                        D[t, s, ps, w] = A[t, ps, w] * B[t + 1, s, w] * numpy.exp(self.get_f(t + 1, s, ps, w))
-            norm = sum(sum(sum(D[t, :, :, :])))
-            D[t, :, :, :] /= norm
-
-    def get_f(self, t, p, ps, w):
-        # Fill cell in forward or backward table.
-        f_list = self.get_fs(t, p, w)
-        f_list += self.get_fss(t, p, ps, w)
-        return sum(self.param[f] for f in f_list)
-
-    def get_f_single(self, t, p, w):
-        # Potential of non-interaction cell
-        f_list = self.get_fs(t, p, w)
-        return sum(self.param[f] for f in f_list)
-
-    def get_fs(self, t, p, w):
-        # Get list of parameters activated at time t, hidden variable p,
-        # and output variable w.
-        S = self.S
-        W = self.W
-        flist = []
-        for f in self.x[t]:
-            flist += [S * S + (p * W + w) + W * S * f]
-        return flist
-
-    def get_fss(self, t, p, ps, w):
-        # Get list of parameters activated at time t, hidden variable p,
-        # previous hidden variable ps, and output variable w.
-        S = self.S
-        return [p * S + ps]
-
-    def train_lmbfgs(self):
-        """
-        Train the model by maximising posterior with LM-BFGS.
-
-        The training data should have been set at this stage:
-            >> h = hcrf(H, maxw, maxf)
-            >> h.X = X
-            >> h.Y = Y
-            >> h.lamb = lamb
-            >> final_params = h.train_lmbfgs()
-        Return the final parameter vector.
-        """
-        initial = self.param[self.param_non_inf_indexes]
-        fparam = fmin_l_bfgs_b(self.get_obj, initial)
-        return fparam
-
-    def get_obj(self, npar, *args):
-        # Use get_deriv to find the objective and its derivative for training.
-        self.param[self.param_non_inf_indexes] = npar
-        self.reset_deriv()
-        for x, y in zip(self.X, self.Y):
-            self.get_deriv(x, y)
-        der = self.der[self.param_non_inf_indexes]
-        ll = -self.ll - (-sum(self.lamb * 0.5 * xx ** 2.0 for xx in npar))
-        der = -der - (-npar * self.lamb)
-        # Print the log-likelihood
-        print self.ll
-        print ll
-        print der
-        return ll, der
-
-    def test(self):
-        """
-        Test the current input data on the current model.
-
-        Prints a confusion matrix, the number of correctly labeled examples,
-        the total number of examples, and accuracy to standard out.
-        """
-        total = 0
-        cor = 0
-        conf = numpy.zeros((self.W, self.W))
-        for x, y in zip(self.X, self.Y):
-            pred = self.load_example(x)
-            py = max(zip(pred, range(len(pred))), key=lambda x: x[0])[1]
-            if py == y:
-                cor += 1
-            conf[y, py] += 1
-            total += 1
-        print conf
-        print cor, total, cor * 1.0 / total
-
-    def label(self):
-        """
-        Predict the labels of current input data and print to standard out.
-        """
-        for x in X:
-            pred = self.load_example(x)
-            py = max(zip(pred, range(len(pred))), key=lambda x: x[0])[1]
-            print py
-
-    def load_params(self, filename):
-        """
-        Take parameters from file and set to current parameters.
-        """
-        f = open(filename, 'r')
-        p = numpy.array([float(line.strip("\n")) for line in f])
-        self.param[self.param_non_inf_indexes] = p
-
-    def save_params(self, filename):
-        """
-        Take current parameters and write to file.
-        """
-        f = open(filename, 'w')
-        for p in self.param[self.param_non_inf_indexes]:
-            f.write(str(p) + "\n")
-        f.close()
+    @staticmethod
+    def _create_default_transitions(num_classes, num_states):
+        num_transitions = num_classes * ((num_states * 2) + 2)
+        transitions = numpy.zeros((num_transitions, 4))
+        counter = 0
+        for transition in range(num_states + 1):
+            for c in range(num_classes):
+                transitions[counter, 0] = c
+                transitions[counter, 1] = transition
+                transitions[counter, 2] = transition + 1
+                transitions[counter, 3] = transition
+                counter += 1
+        return transitions
 
 
-def load_data(filename):
-    """
-    Load training or testing data from file.
+def forward_backward(x, state_parameters, transition_parameters, transitions):
+    x_dot_parameters = numpy.dot(x, state_parameters)
 
-    Return input vectors X, labels Y, cardinality of y in Y, and cardinality
-    of x in X.
-    """
-    f = open(filename, "r")
-    X = []
-    Y = []
-    maxf = 0
-    maxw = 0
-    for line in f:
-        toks = [int(tok) for tok in line.strip(" \n").split(" ")]
-        y = toks[0]
-        if int(y) > maxw:
-            maxw = int(y)
-        x = [[tok] for tok in toks[1:]]
-        for t in toks[1:]:
-            if t > maxf:
-                maxf = t
-        Y += [y]
-        X += [x]
-    return X, Y, maxf + 1, maxw + 1
+    n_time_steps, n_states, n_classes = x_dot_parameters.shape
+    n_transitions, _ = transitions.shape
 
+    # Add extra 1 time steps, one for start state and one for end
+    forward_table = numpy.full((n_time_steps + 1, n_states, n_classes), fill_value=-numpy.inf, dtype='f64')
+    forward_transition_table = numpy.full((n_time_steps + 2, n_states, n_states, n_classes), fill_value=-numpy.inf, dtype='f64')
+    forward_table[0, 0, :] = 0.0
 
-if __name__ == "__main__":
-    usage = """
-    Train, test, or use an HCRF to label input data.
+    for t in range(1, n_time_steps + 1):
+        for transition in range(n_transitions):
+            class_number = transitions[transition, 0]
+            s0 = transitions[transition, 1]
+            s1 = transitions[transition, 2]
+            edge_potential = forward_table[t - 1, s0, class_number] + transition_parameters[transition]
+            forward_table[t, s1, class_number] = numpy.logaddexp(forward_table[t, s1, class_number],
+                                                                 edge_potential + x_dot_parameters[t - 1, s1, class_number])
+            forward_transition_table[t, s0, s1, class_number] = numpy.logaddexp(forward_transition_table[t, s0, s1, class_number],
+                                                                                edge_potential)
 
-    usage: hcrf.py mode datafile paramfile H [lamb]
+    backward_table = numpy.full((n_time_steps + 1, n_states, n_classes), fill_value=-numpy.inf, dtype='f64')
+    backward_table[-1, -1, :] = 0.0
 
-    mode:      Set the script mode to train, tst, or label.
-    datafile:  File containing input datapoints.
-               Format: lines consisting of
-               label 1 [feat1 feat2 ... ] 2
-               where label is a non-negative integer, 1 is the special start
-               of datapoint feature, 2 is the special end of datapoint feature,
-               and feat1, feat2 etc. are integers > 2 representing features
-               activated at the first, second etc. time steps.
-    paramfile: File to store/retrieve parameters.
-    H:         Cardinality of hidden units. Must be >= 3.
-    lamb:      l2 reguralization constant. Only applicable when mode is train.
-    """
-    datafile = sys.argv[2]
-    paramfile = sys.argv[3]
-    H = int(sys.argv[4])
-    if sys.argv[1] == "train":
-        lamb = float(sys.argv[5])
-        X, Y, maxf, maxw = load_data(datafile)
-        h = Hcrf(H, maxw, maxf)
-        h.X = X
-        h.Y = Y
-        h.lamb = lamb
-        final_params = h.train_lmbfgs()
-        h.param[h.param_non_inf_indexes] = final_params[0]
-        h.save_params(paramfile)
+    for t in range(n_time_steps, 0, -1):
+        for transition in range(n_transitions):
+            class_number = transitions[transition, 0]
+            s0 = transitions[transition, 1]
+            s1 = transitions[transition, 2]
+            backward_table[t - 1, s0, class_number] = numpy.logaddexp(backward_table[t - 1, s0, class_number],
+                                                                      backward_table[t, s1, class_number]
+                                                                      + x_dot_parameters[t - 1, s1, class_number]
+                                                                      + transition_parameters[transition])
 
-    if sys.argv[1] == "tst":
-        X, Y, maxf, maxw = load_data(datafile)
-        h = Hcrf(H, maxw, maxf)
-        h.load_params(paramfile)
-        h.X = X
-        h.Y = Y
-        h.test()
+    return forward_table, forward_transition_table, backward_table
 
-    if sys.argv[1] == "label":
-        X, Y, maxf, maxw = load_data(datafile)
-        h = Hcrf(H, maxw, maxf)
-        h.load_params(paramfile)
-        h.X = X
-        h.label()
--- pyhcrf/tests/test_hcrf.py
+++ pyhcrf/tests/test_hcrf.py
@@ -4,6 +4,7 @@ from numpy.testing import assert_array_almost_equal, assert_array_equal
 import numpy as np
 from numpy import random
 from pyhcrf import Hcrf
+from pyhcrf.hcrf import forward_backward
 
 TEST_PRECISION = 3
 
@@ -30,3 +31,56 @@ class TestHcrf(unittest.TestCase):
 
         expected = [0, 1, 0, 1]
         self.assertEqual(actual, expected)
+
+    def test_forward_backward(self):
+        transitions = np.array([[0, 0, 1, 0],
+                                [1, 0, 1, 1],
+                                [0, 1, 1, 2],
+                                [1, 1, 1, 3],
+                                [0, 1, 0, 4],
+                                [1, 1, 0, 5]])
+        transition_parameters = np.array([1, 0, 2, 1, 3, -2])
+        x = np.array([[2, 3],
+                      [1, 0],
+                      [0, 2]])
+        state_parameters = np.array([[[-1, 1],
+                                      [1, -1]],
+                                     [[0, -2],
+                                      [2, 3]]])
+        print 'X.L'
+        print np.dot(x, state_parameters)
+        A = np.zeros((4, 2, 2), dtype='f64')
+        # T  S  W
+        # 5, 2, 2
+        A[0, 0, 0] = 1
+        A[0, 0, 1] = 1
+        A[0, 1, 0] = 0
+        A[0, 1, 1] = 0
+
+        A[1, 0, 0] = 0
+        A[1, 0, 1] = 0
+        A[1, 1, 0] = 1 * np.exp(1) * np.exp(6)
+        A[1, 1, 1] = 1 * np.exp(0) * np.exp(5)
+
+        A[2, 0, 0] = 1 * np.exp(1 + 6) * np.exp(3) * np.exp(-1)
+        A[2, 0, 1] = 1 * np.exp(0 + 5) * np.exp(-2) * np.exp(1)
+        A[2, 1, 0] = 1 * np.exp(1 + 6) * np.exp(2) * np.exp(0)
+        A[2, 1, 1] = 1 * np.exp(0 + 5) * np.exp(1) * np.exp(-2)
+
+        A[3, 0, 0] = 1 * np.exp(1 + 6) * np.exp(2 + 0) * np.exp(3) * np.exp(2)
+        A[3, 0, 1] = 1 * np.exp(0 + 5) * np.exp(1 - 2) * np.exp(-2) * np.exp(-2)
+        A[3, 1, 0] = 1 * np.exp(1 + 6 + 3 - 1) * np.exp(1 + 4) + 1 * np.exp(1 + 6 + 2 + 0) * np.exp(2 + 4)
+        A[3, 1, 1] = 1 * np.exp(0 + 5 - 2 + 1) * np.exp(0 + 6) + 1 * np.exp(0 + 5 + 1 - 2) * np.exp(1 + 6)
+
+        expected_forward_table = np.log(A)
+
+        forward_table, forward_transition_table, backward_table = forward_backward(x,
+                                                                                   state_parameters,
+                                                                                   transition_parameters,
+                                                                                   transitions)
+        #print np.log(A)
+        print (forward_table)
+        print backward_table
+        np.testing.assert_array_almost_equal(forward_table, expected_forward_table)
+        self.assertAlmostEqual(forward_table[-1, -1, 0], backward_table[0, 0, 0], places=5)
+        self.assertAlmostEqual(forward_table[-1, -1, 1], backward_table[0, 0, 1], places=5)
